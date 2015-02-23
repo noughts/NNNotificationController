ae59d84a8b7b73c6097402f69d1d767cd404e5e2
Koichi Yamamoto <noughts@gmail.com>
2015-02-23 13:40:07 +0900

    a

---
+++ Example/NNNotificationController/SecondVC.h
@@ -0,0 +1,13 @@
+//
+//  SecondVC.h
+//  NNNotificationControllerDemo
+//
+//  Created by noughts on 2014/08/19.
+//  Copyright (c) 2014年 noughts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondVC : UIViewController
+
+@end
---
+++ Example/NNNotificationController/SecondVC.m
@@ -0,0 +1,35 @@
+//
+//  SecondVC.m
+//  NNNotificationControllerDemo
+//
+//  Created by noughts on 2014/08/19.
+//  Copyright (c) 2014年 noughts. All rights reserved.
+//
+
+#import "SecondVC.h"
+#import <NNNotificationController.h>
+
+@implementation SecondVC{
+	NNNotificationController* _notificationController;
+}
+
+-(void)awakeFromNib{
+	[super awakeFromNib];
+	
+	_notificationController = [NNNotificationController controllerWithObserver:self];
+	
+	[_notificationController addObserverForName:@"hoge" object:nil queue:nil usingBlock:^(NSNotification *note) {
+		NSLog( @"%@", note );
+	}];
+	
+	[_notificationController addObserverForName:UIApplicationDidBecomeActiveNotification object:nil queue:nil usingBlock:^(NSNotification *note) {
+		NSLog( @"block -> %@", note );
+	}];
+}
+
+
+
+
+
+
+@end
